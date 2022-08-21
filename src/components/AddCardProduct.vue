15ec53f2cc401305e26032bcc1b239e77627c9bf
WIllsnSmith <silverflash150@gmail.com>
2022-08-21 13:28:15 +0300

    add button disabled style and funtional

--- src/components/AddCardProduct.vue
+++ src/components/AddCardProduct.vue
@@ -5,14 +5,20 @@
         Наименование товара
         <div class="dot"></div>
       </label>
-      <input type="text" id="name" placeholder="Введите наименование товара" />
+      <input
+        type="text"
+        id="name"
+        placeholder="Введите наименование товара"
+        v-model="name"
+      />
     </div>
     <div class="add-textarea text">
       <label for="description"> Описание товара </label>
       <textarea
         type="text"
         id="description"
-        placeholder="Введите наименование товара"
+        placeholder="Введите описание товара"
+        v-model="description"
       />
     </div>
     <div class="add-input text">
@@ -20,28 +26,87 @@
         Ссылка на изображение товара
         <div class="dot"></div>
       </label>
-      <input type="text" id="link" placeholder="Введите наименование товара" />
+      <input
+        type="text"
+        id="link"
+        placeholder="Введите ссылку"
+        v-model="link"
+      />
     </div>
     <div class="add-input text">
       <label for="price">
         Цена товара
         <div class="dot"></div>
       </label>
-      <input type="text" id="price" placeholder="Введите наименование товара" />
+      <input
+        type="number"
+        id="price"
+        placeholder="Введите цену"
+        v-model="price"
+      />
     </div>
-    <div class="add-button"><button type="button">Добавить товар</button></div>
+    <div class="add-button">
+      <button
+        type="button"
+        @click="submitForm"
+        :disabled="!isFormValid"
+        :class="{ disabled: !isFormValid }"
+      >
+        Добавить товар
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "AddCardProduct",
-  components: {},
+  data() {
+    return {
+      name: "",
+      description: "",
+      link: "",
+      price: null,
+    };
+  },
+  computed: {
+    isNameValid() {
+      return this.name.length;
+    },
+    isLinkValid() {
+      const linkRegex =
+        /[(http(s)?)://(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/gi;
+      return linkRegex.test(this.link);
+    },
+    isPriceValid() {
+      return this.price;
+    },
+    isFormValid() {
+      return this.isNameValid && this.isLinkValid && this.isPriceValid;
+    },
+  },
+  methods: {
+    submitForm() {
+      const userData = {
+        name: this.name,
+        description: this.description,
+        link: this.link,
+        price: this.price,
+      };
+
+      console.log(userData);
+    },
+  },
 };
 </script>
 
 
 <style scoped lang="scss">
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
 .add-container {
   width: 22rem;
   height: 28rem;
@@ -111,6 +176,11 @@ export default {
       background: #7bae73;
       box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
       border-radius: 10px;
+    }
+    .disabled {
+      background-color: #eeeeee;
+      cursor: not-allowed;
+      color: #b4b4b4;
     }
   }
 }
